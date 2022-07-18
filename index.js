5da51c825031242dc9fe9090ddf25bbd43c24a8d
kralconomoh <102210129+kralconomoh@users.noreply.github.com>
2022-07-18 11:01:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: "odebisiidowu09@gmail.com, tomerpacific@gmail.com",
+    subject: 'Nodemailer Project',
+    text: 'Hi from your nodemailer project, amazing write up, thanks really!'
+  };
+
+  transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
